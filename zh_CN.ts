d4e4cced83deba154321203cb4937b3f5a031985
WANG Guoqin <wangguoqin1001@gmail.com>
2021-09-26 13:45:13 +0800

    updated translation

--- zh_CN.ts
+++ zh_CN.ts
@@ -1,12 +1,42 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="zh_CN">
 <context>
     <name>main</name>
     <message>
-        <location filename="main.qml" line="8"/>
-        <source>Hello World</source>
-        <translation type="unfinished"></translation>
+        <location filename="main.qml" line="10"/>
+        <source>Deep Learning Defect Detection</source>
+        <translation>深度学习缺陷检测软件</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="16"/>
+        <source>Preference</source>
+        <translation>参数配置</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="19"/>
+        <source>Camera State</source>
+        <translation>相机状态</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="22"/>
+        <source>Real Time Detection</source>
+        <translation>实时检测</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="25"/>
+        <source>History</source>
+        <translation>历史溯源</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="28"/>
+        <source>Statistics</source>
+        <translation>报表统计</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="31"/>
+        <source>Data Tagging</source>
+        <translation>数据标注</translation>
     </message>
 </context>
 </TS>
