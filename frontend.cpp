6fc83f44d8b8d37ca958912dac424c7553988ba8
WANG Guoqin <wangguoqin1001@gmail.com>
2021-10-19 20:47:09 +0800

    Correct file path for windows

--- frontend.cpp
+++ frontend.cpp
@@ -5,13 +5,13 @@
 
 const QString frontend::getRecentImages()
 {
-    return "file://" + QFileInfo(globalDB->getRecentImages()).absoluteFilePath();
+    return "file:///" + QFileInfo(globalDB->getRecentImages()).absoluteFilePath();
 }
 
 const QStringList frontend::getRecentImages(const int num)
 {
     QStringList list = globalDB->getRecentImages(num);
     return std::accumulate(list.begin(), list.end(), QStringList(), [](QStringList ret, const QString str) {
-        return ret << "file://" + QFileInfo(str).absoluteFilePath();
+        return ret << "file:///" + QFileInfo(str).absoluteFilePath();
     });
 }
