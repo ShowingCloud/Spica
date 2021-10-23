6e2ae6dacb2d3320d61be40c7ec852e8edc4aab2
WANG Guoqin <wangguoqin1001@gmail.com>
2021-10-24 05:51:24 +0800

    Fixed QNetwork confliction (with Pylon)
    and introduced QML_ELEMENT

--- frontend.h
+++ frontend.h
@@ -1,11 +1,17 @@
 #ifndef FRONTEND_H
 #define FRONTEND_H
 
+#undef interface
+
 #include <QCoreApplication>
+#include <QtQml>
 
 class frontend : public QObject
 {
     Q_OBJECT
+#if QT_VERSION > QT_VERSION_CHECK(5, 15, 0)
+    QML_ELEMENT
+#endif
 
 public:
     explicit frontend(QObject *parent = nullptr) : QObject(parent) {}
